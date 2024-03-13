5b9c182aac3111693a333b48eee290850c8771bb
Lieven Hollevoet <hollie@lika.be>
2024-03-13 21:31:57 +0100

    Allow the user to skip the Caddyfile generation via an environment variable

--- image/start.sh
+++ image/start.sh
@@ -4,10 +4,14 @@ trap 'kill -TERM $PID' TERM INT
 echo "This is Tailscale-Caddy-proxy version"
 tailscale --version
 
-echo "Building Caddy configfile"
+if [ ! -z "$SKIP_CADDYFILE_GENERATION" ] ; then
+   echo "Skipping Caddyfile generation as requested via environment"
+else
+   echo "Building Caddy configfile"
 
-echo $TS_HOSTNAME'.'$TS_TAILNET.'ts.net' > /etc/caddy/Caddyfile
-echo 'reverse_proxy' $CADDY_TARGET >> /etc/caddy/Caddyfile
+   echo $TS_HOSTNAME'.'$TS_TAILNET.'ts.net' > /etc/caddy/Caddyfile
+   echo 'reverse_proxy' $CADDY_TARGET >> /etc/caddy/Caddyfile
+fi
 
 echo "Starting Caddy"
 caddy start --config /etc/caddy/Caddyfile
